55581525047e559c062f3dc24514ef288fb42087
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-05-25 11:19:04 +0530

    feat(url): add data access object (DAO) for URL operations
    - Introduced src/dao/url.dao.js to encapsulate database operations for URL records.
    - Implemented saveShortUrl function to:
      - Persist new short URL entries with original URL and short URL.
      - Optionally attach the user ID for authenticated entries.
      - Handle duplicate errors for both shortUrl and originalUrl with appropriate conflict errors.
    - Developed redirectFromShortUrl function to:
      - Retrieve a URL document by its short URL.
      - Increment the clicks count for tracking purposes.
    - Added findShortUrlBySlug function to query a URL based on a provided slug.
    - Created getAllUrlsByUserId function to retrieve all URLs associated with a given user.
    
    This commit modularizes database interactions, improving maintainability and reusability of URL-related data operations.

---
+++ backend/src/dao/url.dao.js
@@ -0,0 +1,55 @@
+import { ShortUrl as urlModel } from '../models/url.models.js';
+import { ConflictError } from '../utility/nodeUtility.js';
+
+
+export const saveShortUrl = async function (url, randomStringOfShortUrl, userId) {
+    try {
+        const newUrl = new urlModel({
+            originalUrl: url,
+            shortUrl: randomStringOfShortUrl
+        });
+
+        // NOTE - Check for Authenitated User [if authenticated, save user id]
+        if (userId) newUrl.userId = userId
+
+        const savedUrl = await newUrl.save()
+
+        return savedUrl;
+    } catch (error) {
+        console.log('Failed to save in database', error.message);
+
+        // NOTE - Check for Duplicate Short URL
+        if (error.code === 11000) {
+            if (error.message.includes('shortUrl')) {
+                throw new ConflictError('Short URL already exists');
+            }
+
+            if (error.message.includes('originalUrl')) {
+                throw new ConflictError('Original URL already exists');
+            }
+
+        }
+
+        throw error;
+    }
+}
+
+
+export const redirectFromShortUrl = async function (id) {
+    // NOTE - Find and update Short URL [DB Query]
+    const urlResponse = await urlModel.findOneAndUpdate({ shortUrl: id }, {
+        $inc: { clicks: 1 }
+    }, { new: true });
+
+    return urlResponse
+}
+
+export const findShortUrlBySlug = async function (slug) {
+    return await urlModel.findOne({
+        shortUrl: slug
+    });
+}
+
+export const getAllUrlsByUserId = async function (userId) {
+    return await urlModel.find({ userId });
+}
