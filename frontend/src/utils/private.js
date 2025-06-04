9bf11bcec3b3e2c50a62763054a4a107c57d1785
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-06-04 19:39:46 +0530

    feat(private): add route guard utilities for public and private routes
    - Added new file "frontend/src/utils/private.js" to implement route guards for public and private routes.
    - isPublicRoute:
      - Checks authentication status using queryClient.ensureQueryData with the checkAuth API.
      - Dispatches the login action if authenticated and redirects the user to the dashboard.
      - Allows unauthenticated users to access the public route.
    - isPrivateRoute:
      - Verifies the authentication status in a similar manner.
      - Dispatches the login action and confirms auth state in the Redux store.
      - Returns a redirect to the "/auth" route on error or unauthenticated state.
    - Enhances routing security by preventing unauthorized access to protected routes and ensuring a consistent authentication state.

---
+++ frontend/src/utils/private.js
@@ -0,0 +1,67 @@
+import { redirect } from '@tanstack/react-router';
+import { checkAuth } from '../apis/apiCalls.js';
+import { login } from '../store/slice/authSlice.js';
+
+
+export async function isPublicRoute({ context }) {
+    const { store, queryClient } = context;
+
+    try {
+        const data = await queryClient
+            .ensureQueryData({
+                queryKey: ['checkAuth'],
+                queryFn: checkAuth,
+                retry: false
+            })
+
+        if (data?.data) {
+            // NOTE - Authenticated [Redirect User to Dashboard Page]
+
+            // NOTE - Set Store Data and Redirect
+            store.dispatch(login(data.data));
+
+            return redirect({ to: '/dashboard' });
+        }
+
+        // NOTE - Not Authenticated [Allow User to access the route auth]
+        return true;
+
+    } catch (error) {
+        // NOTE - Not Authenticated [Allow User to access the route auth]
+        return true;
+    }
+}
+
+
+export async function isPrivateRoute({ context }) {
+    const { store, queryClient } = context;
+
+    try {
+        const data = await queryClient
+            .ensureQueryData({
+                queryKey: ['checkAuth'],
+                queryFn: checkAuth,
+                retry: false
+            })
+
+        console.log("Check-Auth API Response:", data.data);
+
+
+        if (!data?.data) return false;
+
+        store.dispatch(login(data.data));
+
+        const auth = store.getState().auth;
+
+        console.log("from isPrivateRoute auth: ", auth);
+
+        if (!auth.isAuthenticated) return false;
+
+        // NOTE - Authenticated [Allow User to access the route]
+        return true
+
+    } catch (error) {
+        console.error(error);
+        return redirect({ to: '/auth' });
+    }
+} 
