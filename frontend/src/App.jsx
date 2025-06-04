aeb3310fe7e78742e0192b9c2d7809f1749fddd6
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-06-04 19:38:38 +0530

    feat(App): integrate dynamic routing and authentication state rehydration
    - Updated App.jsx to use Tanstack Router's Outlet for dynamic routing, replacing static component rendering.
    - Integrated Redux with useDispatch and React Query with useQueryClient for authentication state management.
    - Implemented an async rehydration process in a useEffect hook that calls checkAuth to retrieve persisted auth data.
    - Dispatched login or logout actions based on the API's response, ensuring secure and consistent auth state on app load.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import HomePage from './pages/HomePage.jsx'
-import AuthPage from './pages/AuthPage.jsx'
+import React from 'react';
+import { Outlet } from '@tanstack/react-router'
+import { useDispatch } from 'react-redux';
+import { checkAuth } from './apis/apiCalls.js';
+import { login, logout } from './store/slice/authSlice.js';
+import { useQueryClient } from '@tanstack/react-query';
+
 
 
 export default function App() {
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
+
+  React.useEffect(() => {
+    async function reHydrateAuth() {
+      try {
+        const data = await queryClient.fetchQuery({
+          queryKey: ['checkAuth'],
+          queryFn: checkAuth,
+          staleTime: 0,
+          retry: false
+        });
+
+        if (data?.data) {
+          dispatch(login(data.data));
+        } else {
+          dispatch(logout());
+        }
+
+      } catch (error) {
+        console.error("Error re-hydrating auth:", error);
+        dispatch(logout());
+      }
+    };
+
+    reHydrateAuth();
+  }, [dispatch, queryClient]);
+
   return (
     <div
       className='min-h-screen flex items-center justify-center bg-zinc-700'
     >
-      {/* <HomePage /> */}
-      {/* <RegisterPage /> */}
-      <AuthPage />
+      <Outlet />
     </div>
   )
 }
